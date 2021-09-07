c0cf6a7b35c06036319c42904f8f428e22e4144a
Shimron <60049707+ShimronAlakkal@users.noreply.github.com>
2021-09-07 10:28:28 +0530

    Null safe package added

--- lib/adscreen.dart
+++ lib/adscreen.dart
@@ -1,3 +1,4 @@
+import 'package:admob_flutter/admob_flutter.dart';
 import 'package:flutter/material.dart';
 
 class AdScreen extends StatefulWidget {
@@ -17,10 +18,25 @@ class _AdScreenState extends State<AdScreen> {
       ),
       body: ListView.builder(
         itemBuilder: (context, index) {
-          return ListTile();
+          return ListTile(
+            title: AdmobBanner(
+              adSize: AdmobBannerSize.BANNER,
+              adUnitId: "ca-app-pub-3940256099942544/6300978111",
+            ),
+          );
         },
         itemCount: 15,
       ),
     );
   }
 }
+
+
+// App Open	ca-app-pub-3940256099942544/3419835294
+// Banner	ca-app-pub-3940256099942544/6300978111
+// Interstitial	ca-app-pub-3940256099942544/1033173712
+// Interstitial Video	ca-app-pub-3940256099942544/8691691433
+// Rewarded	ca-app-pub-3940256099942544/5224354917
+// Rewarded Interstitial	ca-app-pub-3940256099942544/5354046379
+// Native Advanced	ca-app-pub-3940256099942544/2247696110
+// Native Advanced Video	ca-app-pub-3940256099942544/1044960115
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:app/adscreen.dart';
 import 'package:flutter/material.dart';
 import 'package:admob_flutter/admob_flutter.dart';
 
 void main() {
+  Admob.initialize();
   runApp(
     MaterialApp(
       home: AdScreen(),
